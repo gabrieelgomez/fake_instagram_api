64cd10debc78429a88d6760510d35829a080d975
gabrieelgomez <gagg1707@gmail.com>
2024-05-18 19:59:12 -0400

    ADD: relationships serializer on comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,7 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :commentable, :author
+  attributes :id, :body, :created_at, :author
+
+  def author
+    UserSerializer.new(object.author, scope: scope, root: false)
+  end
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :author, :comments
+  attributes :id, :title, :description, :created_at
+  has_one :author, serializer: UserSerializer
+  has_many :comments, serializer: CommentSerializer
 
   def author
     object.user
